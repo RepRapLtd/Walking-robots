728145359d6e179c4e300e18ca67c265905c5daa
Adrian Bowyer <adrian@reprappro.com>
2023-02-14 15:30:07 +0000

    Web interface WIP.

--- Petoi/Software/Pi/www/Test One Page.php
+++ Petoi/Software/Pi/www/Test One Page.php
@@ -1,4 +1,9 @@
 <HTML>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
 
 <HEAD>
 
@@ -11,8 +16,8 @@
 <script>
 
 $lastServo = 0;
-$servoAngle = 0;
-  
+$lastLeg = 0;
+ 
 function changeServoAngle($delta)
 {
   $call = 'send-command-get-reply.php?args=ChangeServo,' + $lastServo + ',' + $delta;
@@ -27,15 +32,18 @@ function getServoAngle()
   $call = 'send-command-get-reply.php?args=GetServoAngle,' + $lastServo;
   $.get($call, function(data) 
   {
-     $servoAngle = data;
      $('#servo_a').empty().append(data);
   });
 }
 
-
-function setA()
+function getLegData()
 {
-  $angle = document.getElementById("angle").value;
+
+}
+
+
+function setA($angle)
+{
   $call = 'send-command-get-reply.php?args=ChangeServo,' + $lastServo + ',setAngle,' + $angle;
   $.get($call, function(data) 
   {
@@ -50,41 +58,76 @@ function setServo($s)
  getServoAngle();
 }
 
+function setLeg($l)
+{
+ $lastLeg = $l;
+ document.getElementById("legNumber").textContent = $lastLeg;
+ getLegData();
+}
+
+
+function AtStart($s, $l)
+{
+	$lastLeg = $l;
+	setServo($s);
+}
+
 </script>
 
 
 </HEAD>
 
-<BODY>
+<?php
+ $reply = shell_exec("python3 send-command-get-reply.py GetActiveServos");
+ $noServer = "";
+ if($reply == "")
+ {
+  $noServer = "<br><br><H2>The robot server is not running. Start it, then refresh this page.</H2><br><br>";
+ }
+ $servos = explode(" ",$reply);
+ 
+ $reply = shell_exec('python3 send-command-get-reply.py GetLegNames ');
+ $legs = explode(" ",$reply);
+ 
+ $loadFunction = "AtStart(" . $servos[0] . ",'" . $legs[0] . "');";
+
+?>
+
+
+<BODY onload = "<?php echo $loadFunction; ?>" >
+
+
+
 <?php include('header.php'); ?>
 
-<H1>Servos</H1>
+<?php echo $noServer;?>
+
+
+<H2>Servos</H2>
 
 
 <table>
   
 <tr align="left"><th> 
 <p>Select Servo</p>
-<input type="radio" id="0" name="Servo" value="0" checked="checked" onclick="setServo(0);"> 
-<label for="0">0</label> <br>
-<input type="radio" id="1" name="Servo" value="1" onclick="setServo(1);">
-<label for="1">1</label><br>
-<input type="radio" id="6" name="Servo" value="6" onclick="setServo(6);">
-<label for="6">6</label><br>
-<input type="radio" id="7" name="Servo" value="7" onclick="setServo(7);">
-<label for="7">7</label><br>  
-<input type="radio" id="8" name="Servo" value="8" onclick="setServo(8);">
-<label for="8">8</label><br>
-<input type="radio" id="9" name="Servo" value="9" onclick="setServo(9);">
-<label for="9">9</label><br>
-<input type="radio" id="12" name="Servo" value="12" onclick="setServo(12);">
-<label for="12">12</label><br>
-<input type="radio" id="14" name="Servo" value="14" onclick="setServo(14);">
-<label for="14">14</label><br>
-<input type="radio" id="15" name="Servo" value="15" onclick="setServo(15);">
-<label for="15">15</label><br>
 
-</th><th rowspan="1">
+<?php
+ foreach($servos as $servo)
+ {
+   $check = '';
+   if($servo == $servos[0])
+   {
+   	$check = 'checked="checked" ';
+   }
+   if(ctype_print($servo))
+   {
+      echo '<input type="radio" id="'.$servo.'" name="Servo" value="'.$servo.'" '.$check.'onclick="setServo('.$servo.');"> <label for="'.$servo.'">'.$servo.'</label> <br>';
+   }
+ }
+?>
+
+</th>
+<th>
 
 <h2>Servo <span id="servoNumber">0</span></h2>
 
@@ -92,13 +135,13 @@ Angle: <span id="servo_a">0</span><sup>o</sup>
 <br>
 <br>
 
-<button value="Home" > Home </button>
+<button value="Home" onclick="setA(0);" > Home </button>
 <button onclick="changeServoAngle('p1');"> +1 </button>
 <button onclick="changeServoAngle('p10');"> +10 </button>
 <button onclick="changeServoAngle('m1');"> -1 </button>
 <button onclick="changeServoAngle('m10');"> -10 </button>
 <br><br><br>
-<button onclick="setA()">Set angle to </button><input type="text" id="angle" size="4">
+<button onclick="setA(document.getElementById('angle').value);">Set angle to</button> <input type="text" id="angle" size="4">
 
 <br><br><br>
 
@@ -117,74 +160,41 @@ Angle: <span id="servo_a">0</span><sup>o</sup>
 
 <br>
 <br>
-<button type="submit" value="Home" > Zero Servos </button> <button type="submit" value="Home" > Save current position as zeros </button> <!-- Need to set what the buttons do as currently same as "Home" Button below -->
+<button type="submit" value="Home" > Zero Servos </button> <button type="submit" value="Home" > Save current position as zeros </button> 
 
 
-
-
-
-
-<script>
-   window.addEventListener("load", () => {
-  function sendData() {
-
-$button=$_POST['action'];
-
-    const XHR = new XMLHttpRequest();
-
-    // Bind the FormData object and the form element
-    const FD = new FormData(form);
-
-    // Define what happens on successful data submission
-    XHR.addEventListener("load", (event) => {
-      alert(event.target.responseText);
-    });
-
-    // Define what happens in case of error
-    XHR.addEventListener("error", (event) => {
-      alert('Oops! Something went wrong.');
-    });
-
-    // Set up our request
-    XHR.open("POST", "http://192.168.68.192/receive.php?b=".$button);
-
-    // The data sent is what the user provided in the form
-    XHR.send(FD);
-  }
-
-  // Get the form element
-  const form = document.getElementById("myForm");
-
-  // Add 'submit' event handler
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    sendData();
-  });
-});
-  </script>
-</a>
-
-
+<br><br><hr>
 <H2>Legs</H2>
 
-<?php 
- $output = shell_exec('python3 send-command-get-reply.py GetLegNames '.$servo );
- $tokens = explode(" ",$output);
-?> 
+
 
 <table>
   <tr align="left">
-   <th><a href="leg.php?l=fl"><?php echo $tokens[0]; ?> </a></th>
-   <th><a href="leg.php?l=fr"><?php echo $tokens[1]; ?></a></th>
-  </tr>
-  
-  <tr align="left">
-   <th><a href="leg.php?l=bl"><?php echo $tokens[2]; ?></a></th>
-   <th><a href="leg.php?l=br"><?php echo $tokens[3]; ?></a></th>
+   <th> 
+<p>Select Leg</p>
+
+<?php
+ foreach($legs as $leg)
+ {
+   $check = '';
+   if($leg == $legs[0])
+   {
+   	$check = 'checked="checked" ';
+   }
+   if(ctype_print($leg))
+   {
+      echo '<input type="radio" id="'.$leg.'" name="Leg" value="'.$leg.'" '.$check.'onclick="setLeg('.$leg.');"> <label for="'.$leg.'">'.$leg.'</label> <br>';
+   }
+ }
+?>
+
+</th>
   </tr>
  
 </table>
+
+<br><br><hr>
+
 <H2>Camera</H2>
 
 <img src="testpic.jpg" alt="robot camera image" width="500">
