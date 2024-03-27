ce6fab3aa0f5935341ac1b38f9307a8b97167726
Adrian Bowyer <adrian@reprappro.com>
2024-03-27 12:25:55 +0000

    Trying (!) to combine the camera and robot servers into one.

--- Petoi/Software/Pi/www/petoi-server.py
+++ Petoi/Software/Pi/www/petoi-server.py
@@ -43,6 +43,13 @@
 # For socket info see:
 # https://docs.python.org/3/library/socketserver.html#socketserver-tcpserver-example
 #
+# The Raspberry Pi Camera server was written by GPT4, not me.
+#
+# The camera stream can be embedded in a web page with
+#
+# <?php include 'camera_ip.php'; ?>
+# <img src="http://<?= $camera_ip; ?>:8000/stream.mjpg" width="640" height="480">
+#
 ############################################################################################
 
 
@@ -53,8 +60,15 @@ debug = True
 import sys
 import time
 import math as maths
+from flask import Flask, Response
+import picamera
+import io
+from time import sleep
+import socket
 import socketserver
+import threading
 
+# Are we running as a simulator, or for real?
 
 if len(sys.argv) == 1:
  import rrlpetoi as rrlp
@@ -65,15 +79,69 @@ else:
  sys.exit(1)
 
 
-#from tendo import singleton
+# Set up the robot
 
 encoding = "utf-8"
 
 robot = rrlp.Robot()
 if debug:
  print("Robot initialised")
+ 
+#########################################################################################
+
+# The camera server
+
+app = Flask(__name__)
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # Doesn't need to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+def generate_php_script(ip_address):
+    php_content = f"""<?php
+// Auto-generated IP address for camera stream
+$camera_ip = "{ip_address}";
+?>
+"""
+    with open("camera_ip.php", "w") as php_file:
+        php_file.write(php_content)
+
+def generate_camera_stream():
+    with picamera.PiCamera() as camera:
+        # Camera warm-up time
+        sleep(2)
+
+        stream = io.BytesIO()
+        for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+            stream.seek(0)
+            frame = stream.read()
+
+            # Use a multipart response format (MJPEG)
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            stream.seek(0)
+            stream.truncate()
+
+@app.route('/stream.mjpg')
+def stream_mjpg():
+    return Response(generate_camera_stream(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def runCameraServer():
+  app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
 
 
+#########################################################################################
+
+# The robot server
 
 def GetLegNames():
  reply = ""
@@ -246,9 +314,20 @@ class TCPHandler(socketserver.StreamRequestHandler):
    print("sent: "+reply)
   self.wfile.write(bytes(reply + "\n", encoding))
 
-if __name__ == "__main__":
+# Launch the two servers
 
- #me = singleton.SingleInstance() # will sys.exit(-1) if other instance is running
+if __name__ == '__main__':
+
+# Camera
+
+    ip_address = get_ip_address()
+    generate_php_script(ip_address)
+    if debug:
+      print('Starting camera server')
+    cameraThread = threading.Thread(target=runCameraServer)
+    cameraThread.start()
+
+# Robot
 
  # Create the robot server, binding to localhost on port 9999
  HOST, PORT = "localhost", 9999
